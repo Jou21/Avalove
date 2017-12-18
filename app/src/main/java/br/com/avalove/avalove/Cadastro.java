4bffc2c8e8437fab2a1f8a28967e54b85e765417
João Victor <jvsb21@gmail.com>
2017-12-18 20:04:40 -0200

    Ajustes no checkbox da tela login

--- app/src/main/java/br/com/avalove/avalove/Cadastro.java
+++ app/src/main/java/br/com/avalove/avalove/Cadastro.java
@@ -1,10 +1,18 @@
 package br.com.avalove.avalove;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.TypefaceSpan;
 import android.view.WindowManager;
+import android.widget.EditText;
 
 public class Cadastro extends AppCompatActivity {
+
+    private EditText edtPassword1,edtPassword2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +20,19 @@ public class Cadastro extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Editando fonte do password
+        edtPassword1 = (EditText) findViewById(R.id.edt_cadastro_password1);
+        edtPassword2 = (EditText) findViewById(R.id.edt_cadastro_password2);
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/comic.ttf");
+        edtPassword1.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        edtPassword2.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        TypefaceSpan typefaceSpan = new CustomTypefaceSpan(custom_font);
+        SpannableString spannableString = new SpannableString("");
+        spannableString.setSpan(typefaceSpan, 0, spannableString.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        edtPassword1.setHint(spannableString);
+        edtPassword2.setHint(spannableString);
+        edtPassword1.setTypeface(custom_font);
+        edtPassword2.setTypeface(custom_font);
     }
 }
--- app/src/main/java/br/com/avalove/avalove/Login.java
+++ app/src/main/java/br/com/avalove/avalove/Login.java
@@ -1,15 +1,22 @@
 package br.com.avalove.avalove;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.TypefaceSpan;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
     private Button btnSignIn;
+    private EditText edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,16 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Editando fonte do password
+        edtPassword = (EditText) findViewById(R.id.edt_password);
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/comic.ttf");
+        edtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        TypefaceSpan typefaceSpan = new CustomTypefaceSpan(custom_font);
+        SpannableString spannableString = new SpannableString("");
+        spannableString.setSpan(typefaceSpan, 0, spannableString.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        edtPassword.setHint(spannableString);
+        edtPassword.setTypeface(custom_font);
 
         btnSignIn = (Button) findViewById(R.id.btn_sign_in);
 
